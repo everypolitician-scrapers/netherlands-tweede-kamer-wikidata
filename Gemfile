d892ad8fd43c02f11e4f530ba5dd84c4d53c9cdc
Chris Mytton <chrismytton@gmail.com>
2015-10-30 17:43:53 +0000

    Initial scraper

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
-# It's easy to add more libraries or choose different versions. Any libraries
-# specified here will be installed and made available to your morph.io scraper.
-# Find out more: https://morph.io/documentation/ruby
+source 'https://rubygems.org'
 
-source "https://rubygems.org"
-
-ruby "2.0.0"
-
-gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
-gem "mechanize"
+gem 'scraperwiki', github: 'openaustralia/scraperwiki-ruby', branch: 'morph_defaults'
+gem 'wikidata-fetcher', github: 'everypolitician/wikidata-fetcher'
+gem 'colorize'
+gem 'mediawiki_api'
+gem 'nokogiri'
+gem 'open-uri-cached'
+gem 'pry'
+gem 'rest-client'
--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,40 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'bundler/setup'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'scraperwiki'
+require 'wikidata/fetcher'
+require 'nokogiri'
+require 'open-uri/cached'
+require 'rest-client'
+require 'pry'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+OpenURI::Cache.cache_path = '.cache'
+
+def noko_for(url)
+  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read)
+end
+
+def wikinames_from(url)
+  noko = noko_for(url)
+  names = noko.xpath('//div[@id="mw-content-text"]//h3/following-sibling::div[@class="editmode"]/ul//a[contains(@href, "/wiki/") and not(@class="new")]/@title').map(&:text)
+  abort 'No names' if names.count.zero?
+  names
+end
+
+def fetch_info(names)
+  WikiData.ids_from_pages('nl', names).each do |name, id|
+    puts "Fetching #{name} #{id}"
+    data = WikiData::Fetcher.new(id: id).data('nl')
+    unless data
+      warn "No data for #{name} #{id}"
+      next
+    end
+    data[:original_wikiname] = name
+    ScraperWiki.save_sqlite([:id], data)
+  end
+end
+
+names = wikinames_from('https://nl.wikipedia.org/wiki/Samenstelling_Tweede_Kamer_2012-heden')
+
+fetch_info names.uniq
+
+warn RestClient.post(ENV['MORPH_REBUILDER_URL'], {}) if ENV['MORPH_REBUILDER_URL']
