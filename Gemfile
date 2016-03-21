04fd26d9e9efafbdfceef8976d68fb25768fd438
Chris Mytton <chrismytton@gmail.com>
2016-03-21 16:20:40 +0100

    Update to the new more concise wikidata approach

--- Gemfile
+++ Gemfile
@@ -6,14 +6,7 @@ source "https://rubygems.org"
 
 ruby "2.0.0"
 
-gem "colorize"
-gem "mediawiki_api"
-gem "nokogiri"
-gem "open-uri-cached"
 gem "pry"
-gem "rest-client"
-gem "sqlite_magic", :git => "https://github.com/openc/sqlite_magic.git"
 gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
 gem "wikidata-fetcher", '>=0.10.2', git: "https://github.com/everypolitician/wikidata-fetcher.git"
 gem 'wikisnakker', git: 'https://github.com/everypolitician/wikisnakker', branch: 'master'
-
--- scraper.rb
+++ scraper.rb
@@ -1,44 +1,9 @@
 require 'bundler/setup'
-
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'nokogiri'
-require 'open-uri/cached'
-require 'rest-client'
-require 'pry'
-
-OpenURI::Cache.cache_path = '.cache'
-
-def noko_for(url)
-  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read)
-end
-
-def wikinames_from(url)
-  noko = noko_for(url)
-  # Get all ULs in the 'Samenstelling' section
-  #   http://discomposer.com/scraping-tricks-nodes-between-other-nodes/
-  node = noko.xpath('//h2[contains(.,"Samenstelling")]').first
-  uls = node.xpath('following::ul | following::h2').slice_before { |e| e.name == 'h2' }.first
-  names = uls.map { |ul| ul.xpath('.//li//a[contains(@href, "/wiki/") and not(@class="new")]/@title').map(&:text) }.flatten
-  abort 'No names' if names.count.zero?
-  names
-end
-
-def fetch_info(names)
-  WikiData.ids_from_pages('nl', names).each do |name, id|
-    puts "Fetching #{name} #{id}"
-    data = WikiData::Fetcher.new(id: id).data('nl')
-    unless data
-      warn "No data for #{name} #{id}"
-      next
-    end
-    data[:original_wikiname] = name
-    ScraperWiki.save_sqlite([:id], data)
-  end
-end
-
-names = wikinames_from('https://nl.wikipedia.org/wiki/Samenstelling_Tweede_Kamer_2012-heden')
-
-fetch_info names.uniq
-
-warn RestClient.post(ENV['MORPH_REBUILDER_URL'], {}) if ENV['MORPH_REBUILDER_URL']
+names = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://nl.wikipedia.org/wiki/Samenstelling_Tweede_Kamer_2012-heden',
+  after: '//h2[contains(.,"Samenstelling van")]',
+  before: '//span[@id="Bijzonderheden"]',
+  xpath: './/li//a[not(@class="new")]/@title',
+)
+EveryPolitician::Wikidata.scrape_wikidata(names: { nl: names })
