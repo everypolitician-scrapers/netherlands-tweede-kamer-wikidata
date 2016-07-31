d969c3efb9c21b140296952564b474b1c15c6b4b
Tony Bowden <tony@tmtm.com>
2016-07-31 09:52:07 +0100

    Also scrape EN

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,17 @@
-require 'bundler/setup'
 require 'wikidata/fetcher'
-names = EveryPolitician::Wikidata.wikipedia_xpath(
+
+nl_names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://nl.wikipedia.org/wiki/Samenstelling_Tweede_Kamer_2012-heden',
   after: '//h2[contains(.,"Samenstelling van")]',
   before: '//span[@id="Bijzonderheden"]',
   xpath: './/li//a[not(@class="new")]/@title',
 )
-EveryPolitician::Wikidata.scrape_wikidata(names: { nl: names })
+
+en_names = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/Members_of_the_House_of_Representatives_of_the_Netherlands,_2012%E2%80%93present',
+  after: '//h2[contains(.,"Parties")]',
+  before: '//span[@id="References"]',
+  xpath: './/li//a[not(@class="new")]/@title',
+)
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { nl: nl_names, en: en_names })
