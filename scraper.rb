527968f254f7e5f165b0abbf6f212b85c0302707
Tony Bowden <tony@tmtm.com>
2017-04-11 12:00:54 +0100

    Update for end-of-term page moves

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,14 @@
 require 'wikidata/fetcher'
 
 nl_names = EveryPolitician::Wikidata.wikipedia_xpath(
-  url: 'https://nl.wikipedia.org/wiki/Samenstelling_Tweede_Kamer_2012-heden',
-  after: '//h2[contains(.,"Samenstelling van")]',
+  url: 'https://nl.wikipedia.org/wiki/Samenstelling_Tweede_Kamer_2012-2017',
+  after: '//h2[contains(.,"Gekozen bij de verkiezingen van 12 september 2012")]',
   before: '//span[@id="Bijzonderheden"]',
   xpath: './/li//a[not(@class="new")]/@title',
 )
 
 en_names = EveryPolitician::Wikidata.wikipedia_xpath(
-  url: 'https://en.wikipedia.org/wiki/Members_of_the_House_of_Representatives_of_the_Netherlands,_2012%E2%80%93present',
+  url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_House_of_Representatives_of_the_Netherlands,_2012–17',
   after: '//h2[contains(.,"Parties")]',
   before: '//span[@id="References"]',
   xpath: './/li//a[not(@class="new")]/@title',
